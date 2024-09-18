e211359712f079802d856a3ec9d5e50a0c60a94e
Omar Crosby <omar.crosby@gmail.com>
2024-09-18 06:45:25 -0400

    chore: wip

--- ratings/services/db/country.py
+++ ratings/services/db/country.py
@@ -2,6 +2,15 @@
 from sqlmodel import Session, select
 from ratings.models.country import Country
 
+
+def get_countries(session: Session) -> list[Country]:
+    statement = select(Country)
+    return session.exec(statement).all()
+
+def get_country_by_id(session: Session, country_id: int) -> Country:
+    statement = select(Country).where(Country.id == country_id)
+    return session.exec(statement).first()
+
 def get_country_by_name(session: Session, name: str) -> Country:
     statement = select(Country).where(Country.name == name)
     return session.exec(statement).first()
